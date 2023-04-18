89dd6c57f20345884da4cfebc807fc2f272849df
Pawan kumar <91398600+pawankumar10306@users.noreply.github.com>
2023-04-18 22:30:22 +0530

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app=Flask(__name__)
-    app.config['SECRET KEY']='asdfghjkl;'
+    #app.config['SECRET KEY']='asdfghjkl;'
     
     from .views import views
     
